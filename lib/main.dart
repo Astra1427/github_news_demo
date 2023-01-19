a9f1510e07e53f520f8c9fa249173f5865fcbc8c
Ling <1427917847@qq.com>
2023-01-19 14:45:19 +0800

    main init

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:github_news_demo/net/github.dart';
+import 'package:github_news_demo/net/github_services.dart';
+import 'package:github_news_demo/pages/home_feed.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+  const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
-
-    return const Placeholder();
+    String userName = 'Astra1427';
+    String token = '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';
+    GithubServices.init(Github(token));
+    return MaterialApp(
+      title: 'Github 技术头条',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: const HomeFeedPage(),
+    );
   }
+
 }
